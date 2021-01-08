35ad972a6772210504a06a8b0b8e7a20b7247e7b
y-katsuoka <golden_duck2@hotmail.com>
2021-01-09 03:18:14 +0900

    update

--- Assets/Scripts/GenarateGrass.cs
+++ Assets/Scripts/GenarateGrass.cs
@@ -17,14 +17,17 @@ public class GenarateGrass : MonoBehaviour
     {
         var grasses = new Mesh();
 
-        var proceduralGrassGeometry = new ProceduralGrassGeometry(grassWidth, grassHeight, grassSplitCnt);
-        var geo = proceduralGrassGeometry.CreateGrass(Vector3.zero, 0);
+        var proceduralGrassGeometry = new ProceduralGrassGeometry(grassWidth, grassHeight, grassSplitCnt, 0.2f);
 
-        Debug.Log(geo.verticesList.Count());
+        proceduralGrassGeometry.CreateChain(Vector3.zero, Quaternion.AngleAxis(90f, Vector3.up));
+        proceduralGrassGeometry.CreateChain(Vector3.one, Quaternion.AngleAxis(30f, Vector3.up));
+        proceduralGrassGeometry.CreateChain(Vector3.right, Quaternion.AngleAxis(0f, Vector3.up));
 
-        grasses.vertices = geo.verticesList.ToArray();
-        grasses.triangles = geo.trianglesList.ToArray();
-        grasses.SetUVs(0, geo.uvList.ToArray());
+        // Debug.Log(geo.verticesList.Count());
+
+        grasses.vertices = proceduralGrassGeometry.verticesChainList.ToArray();
+        grasses.triangles = proceduralGrassGeometry.trianglesChainList.ToArray(); // geo.trianglesList.ToArray();
+        grasses.SetUVs(0, proceduralGrassGeometry.uvChainList.ToArray());
 
         grasses.RecalculateNormals();
         grasses.RecalculateBounds();
--- Assets/Scripts/ProceduralGrass.cs
+++ Assets/Scripts/ProceduralGrass.cs
@@ -9,16 +9,17 @@ public class ProceduralGrassGeometry
     List<Vector2> uvList = new List<Vector2>();
     List<int> trianglesList = new List<int>();
 
-    public ProceduralGrassGeometry(float grassWidth, float grassHeight, int grassSplitCnt)
+    public ProceduralGrassGeometry(float grassWidth, float grassHeight, int grassSplitCnt, float bending)
     {
         var unitHeight = grassHeight / (float)grassSplitCnt;
+        var Unitbending = bending / ((float)grassSplitCnt * (float)grassSplitCnt);
 
         for (int i = 0; i < grassSplitCnt; i++)
         {
             verticesList.AddRange(new Vector3[]
                 {
-                        new Vector3(0f          ,unitHeight * i         ,0f) ,
-                        new Vector3(grassWidth  ,unitHeight * i         ,0f) ,
+                        new Vector3(0f          ,unitHeight * i         , i * i * Unitbending) ,
+                        new Vector3(grassWidth  ,unitHeight * i         , i * i * Unitbending) ,
                 });
             uvList.AddRange(new Vector2[]
                 {
@@ -29,7 +30,7 @@ public class ProceduralGrassGeometry
         }
 
         // 草の頂
-        verticesList.Add(new Vector3(grassWidth / 2f, unitHeight * grassSplitCnt, 0f));
+        verticesList.Add(new Vector3(grassWidth / 2f, unitHeight * grassSplitCnt, bending));
         uvList.Add(new Vector2(1f, 1f));
 
         for (int i = 0; i < grassSplitCnt - 1; i++)
@@ -52,14 +53,33 @@ public class ProceduralGrassGeometry
                 });
     }
 
-
-    public (IEnumerable<Vector3> verticesList, IEnumerable<Vector2> uvList, IEnumerable<int> trianglesList) CreateGrass(Vector3 worldPosision, int VerticesOffset)
+    public (IEnumerable<Vector3> verticesList, IEnumerable<Vector2> uvList, IEnumerable<int> trianglesList) CreateGrass(Vector3 worldPosision, Quaternion rotaion, int VerticesOffset)
     {
         return (
-            verticesList.Select(xx => xx + worldPosision)
+            verticesList.Select(xx => rotaion * xx + worldPosision)
             , uvList.Select(xx => xx)
             , trianglesList.Select(xx => xx + VerticesOffset)
         );
     }
 
+
+    public List<Vector3> verticesChainList = new List<Vector3>();
+    public List<int> trianglesChainList = new List<int>();
+    public List<Vector2> uvChainList = new List<Vector2>();
+
+    public void CreateChain(Vector3 worldPosision, Quaternion rotaion)
+    {
+        var geo = CreateGrass(worldPosision, rotaion, verticesChainList.Count());
+
+        verticesChainList.AddRange(geo.verticesList);
+        trianglesChainList.AddRange(geo.trianglesList);
+        uvChainList.AddRange(geo.uvList);
+    }
+
+    public void ResetChain()
+    {
+        verticesChainList = null;
+        trianglesChainList = null;
+        uvChainList = null;
+    }
 }
