5902fd2d923ecf9cf57892bcfe38b53c9088969a
y-katsuoka <golden_duck2@hotmail.com>
2021-01-11 05:51:07 +0900

    update

--- Assets/Scripts/GenarateGrass.cs
+++ Assets/Scripts/GenarateGrass.cs
@@ -11,17 +11,21 @@ public class GenarateGrass : MonoBehaviour
     public int grassSplitCnt = 3;
     public float grassHeight = 0.3f;
     public float grassWidth = 0.01f;
+    public float bend = 0.1f;
+    public float distance = 0.03f;
+
+    public Vector2 size = new Vector2(50, 50);
 
 
     void Start()
     {
-        var proceduralGrassGeometry = new ProceduralGrassGeometry(grassWidth, grassHeight, grassSplitCnt, 0.1f);
+        var proceduralGrassGeometry = new ProceduralGrassGeometry(grassWidth, grassHeight, grassSplitCnt, bend);
 
-        foreach (var x in Enumerable.Range(0, 50))
+        foreach (var x in Enumerable.Range(0, (int)size.x))
         {
-            foreach (var z in Enumerable.Range(0, 50))
+            foreach (var z in Enumerable.Range(0, (int)size.y))
             {
-                proceduralGrassGeometry.CreateChain(new Vector3(x * 0.03f, 0f, z * 0.03f), Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.up), Vector3.one * Mathf.Clamp01(Mathf.PerlinNoise(x, z) + 0.1f)); //* 0.5f + 0.5f));
+                proceduralGrassGeometry.CreateChain(new Vector3(x * distance, 0f, z * distance), Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.up), Vector3.one * Mathf.Clamp01(Mathf.PerlinNoise(x, z) + 0.1f)); //* 0.5f + 0.5f));
             }
         }
 
